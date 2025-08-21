27cd360195f384785ba6d8be2d9f2950e8325867
Kaua-Navarro <kaua.navarro@sptech.school>
2025-08-21 19:38:31 -0300

    ADD: 5/5 Utils

--- public/js/utils/utils_cliente_pages.js
+++ public/js/utils/utils_cliente_pages.js
@@ -1,8 +1,9 @@
-// export function navegar(path) {
-//     window.location.href = path;
-// }
+// Util - 1 - Redirecionar para página do botão.
+export function navegar(path) {
+    window.location.href = path;
+}
 
-// // utils.js
+// Util - 2 - Formatar nome (Letra Maiúscula).
 export function formatarNomeInput(inputElement) {
   function nomeFormatado(value) {
     return value
@@ -21,6 +22,43 @@ export function formatarNomeInput(inputElement) {
   inputElement.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       formatarNome();
+    }
+  });
+}
+
+// Util - 3 - Validar confirmação de senha.
+export function validarSenha(senha1, senha2) {
+    if (senha1 = senha2) {
+        return 'As senhas coincidem.'
+    } else {
+        return 'As senhas não coincidem'
+    }
+}
+
+// Util - 4 - Chamar Pop Up
+export function abrirPopUp(elemento1, elemento2) {
+    elemento1.style.display = 'none';
+    elemento2.style.display = 'block';
+}
+
+// Util - 5 - Formatar CPF
+export function formatarCPFInput(inputElement) {
+  function formatarCPF(value) {
+    const cpf = value.replace(/\D/g, "");
+    
+    if (cpf.length == 11) {
+        return cpf;
+    }
+  }
+
+  function formatar() {
+    inputElement.value = formatarCPF(inputElement.value);
+  }
+
+  inputElement.addEventListener("blur", formatar);
+  inputElement.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      formatar();
     }
   });
 }
