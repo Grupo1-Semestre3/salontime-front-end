cf0deeea04adddec3a81f564800b2439dc901d74
Caio Acayaba Furtado <caio.furtado@sptech.school>
2025-08-22 23:38:07 -0300

    ADD: pages controle mensal

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// ADICIONAR NO ARQUIVO HTML 
+// <script type="module" src="../js/main.js"></script>
+import {
+  navegar,
+  formatarNomeInput,
+  validarSenha,
+  abrirPopUp,
+  formatarCPFInput
+} from './utils/utils_cliente_pages.js';
+
+// navegar(path)
